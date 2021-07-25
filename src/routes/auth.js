cac52f93edb517d4c39ca62ecd5736221e6465b5
Mark Sauer <39628206+sauerm1@users.noreply.github.com>
2021-07-24 23:41:08 -0400

    add login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,9 +10,23 @@ router.get("/users", async (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-    const { username, password } = req.body
-	const result = await auth.signup(username, password);
-	res.status(200).json(result);
+    try {
+        const { username, password } = req.body
+        const result = await auth.signup(username, password);
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const result = await auth.login(username, password);
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 export default router;
